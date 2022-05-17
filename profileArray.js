af59c1d1f020a1c3dcd39a0cbf1327f3ab0ced5f
jozi <johan.b.strydom@gmail.com>
2022-05-17 17:01:10 +0200

    Update profileArray.js
    Update defaults

--- profileArray.js
+++ profileArray.js
@@ -1,18 +1,21 @@
 var process = require('process');
 
 let lengthCalls, elementCalls;
-const array = [];
 
 //Enter array lenght (or as argument)
 const n = process.argv[2] ? process.argv[2] : 100;
 
-//Build up random array with duplicates
-for (let i = 0; i < n; i++) {
-    if (i % 3 == 0)
-        array[i] = 3
-    else
-        array[i] = i
-}
+//Set your own array (if not set, array will be built up below)
+const array = [];
+
+//Build up array with duplicates (3), if array not set (crap but not profiled)
+if (array.length == 0)
+    for (let i = 0; i < n; i++) {
+        if (i % 3 == 0)
+            array[i] = 3
+        else
+            array[i] = i
+    }
 
 //Proxy to track element requests
 const proxy1 = new Proxy(array, {
@@ -32,7 +35,7 @@ let duplicates;
 
 const callbacks = [
     {
-        name: 'Duplicates by set and filter',
+        name: 'Get duplicates by set and filter',
         callback: (arr) => {
             const set = new Set(arr);
             duplicates = arr.filter((i) => {
@@ -45,14 +48,32 @@ const callbacks = [
         }
     },
     {
-        name: 'Duplicates by filter and index',
+        name: 'Get duplicates by set and filter',
+        callback: (arr) => {
+            duplicates = [];
+
+            let grouped = {};
+            arr.forEach((e) => {
+                if (!grouped[e])
+                    grouped[e] = true;
+                else {
+                    duplicates.push(e);
+                }
+            });
+
+
+            console.log(`Duplicates: ${[...new Set(duplicates)]}`);
+        }
+    },
+    {
+        name: 'Get duplicates by filter and index',
         callback: (arr) => {
             duplicates = arr.filter((i, x) => arr.indexOf(i) !== x);
             console.log(`Duplicates: ${[...new Set(duplicates)]}`);
         }
     },
     {
-        name: 'Duplicates by for loop and filter',
+        name: 'Get duplicates by for loop and filter (slow AF!)',
         callback: (arr) => {
             duplicates = [];
             for (let i = 0; i < arr.length; i++) {
@@ -61,6 +82,12 @@ const callbacks = [
                     duplicates[duplicates.length] = i;
             }
             console.log(`Duplicates: ${[...new Set(duplicates)]}`);
+        }
+    },
+    {
+        name: 'Some text to describe the function',
+        callback: (arr) => {
+            //Add function code here
         }
     }
 ]
@@ -79,7 +106,7 @@ callbacks.forEach((c, i) => {
     c.callback(proxy1);
     endTime = new Date().getTime();
 
-    console.log(`Array element requests: ${elementCalls}`);
-    console.log(`Array lenght requests: ${lengthCalls}`);
+    console.log(`Array element request count: ${elementCalls}`);
+    console.log(`Array length request count: ${lengthCalls}`);
     console.log(`Duration: ${(endTime - startTime) / 1000} sec`);
 })
